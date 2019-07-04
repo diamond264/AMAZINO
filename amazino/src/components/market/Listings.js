03c4ab4ecd980176b2d1d17bd94b6a9697d247f4
redstoneruin <ryansteinwert@gmail.com>
2019-07-03 23:29:37 -0700

    Market data type and data update

--- amazino/src/components/market/Listings.js
+++ amazino/src/components/market/Listings.js
@@ -3,22 +3,58 @@ import React from 'react';
 import ListingSummary from './ListingSummary';
 
 const Listings = () => {
-    var testData = {
-        title: "Title",
-        displayName: "rsteinwe",
-        price: 30,
-        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores commodi vel veritatis voluptates, nemo ratione, illo impedit magni dolor excepturi odit pariatur odio delectus rem labore autem error provident illum!",
-        createdOn: new Date(),
-        id: 20
-    }
+    var testData = [
+        {
+            title: "Bike",
+            displayName: "rsteinwe",
+            price: 20,
+            dueDate: new Date(),
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores commodi vel veritatis voluptates, nemo ratione, illo impedit magni dolor excepturi odit pariatur odio delectus rem labore autem error provident illum!",
+            createdOn: new Date()
+        },
+        {
+            title: "Duck",
+            displayName: "rsteinwe",
+            price: 15,
+            dueDate: new Date(),
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores commodi vel veritatis voluptates, nemo ratione, illo impedit magni dolor excepturi odit pariatur odio delectus rem labore autem error provident illum!",
+            createdOn: new Date()
+        },
+        {
+            title: "Crocodile",
+            displayName: "rsteinwe",
+            price: 30,
+            dueDate: new Date(),
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores commodi vel veritatis voluptates, nemo ratione, illo impedit magni dolor excepturi odit pariatur odio delectus rem labore autem error provident illum!",
+            createdOn: new Date()
+        },
+        {
+            title: "Toast",
+            displayName: "rsteinwe",
+            price: 27,
+            dueDate: new Date(),
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores commodi vel veritatis voluptates, nemo ratione, illo impedit magni dolor excepturi odit pariatur odio delectus rem labore autem error provident illum!",
+            createdOn: new Date()
+        },
+        {
+            title: "Apples",
+            displayName: "rsteinwe",
+            price: 250,
+            dueDate: new Date(),
+            description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores commodi vel veritatis voluptates, nemo ratione, illo impedit magni dolor excepturi odit pariatur odio delectus rem labore autem error provident illum!",
+            createdOn: new Date()
+        }
+    ]
 
     return(
         <div className="row">
-            <ListingSummary  {...testData}/>
-            <ListingSummary  {...testData}/>
-            <ListingSummary  {...testData}/>
-            <ListingSummary  {...testData}/>
-            <ListingSummary  {...testData}/>
+            {
+                testData && testData.map(item => {
+                    return (
+                        <ListingSummary {...item} />
+                    )
+                })
+            }
         </div>
     )
 }
