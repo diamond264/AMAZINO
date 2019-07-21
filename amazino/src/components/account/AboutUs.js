749d533b7e2dc25e513c93a2794f75ae2fd69ae7
Yu Xi Ma <yma71@ucsc.edu>
2019-07-20 17:14:22 -0700

    Updating the About Us page

--- amazino/src/components/account/AboutUs.js
+++ amazino/src/components/account/AboutUs.js
@@ -14,11 +14,26 @@ class AboutUs extends Component {
                         <div className="divider"></div>
                         <div className="row">
                             <h5>The Team Behind Amazino</h5>
-                            <p>Insert info about our team here</p>
+                            <p>
+                                We are UCSC students who created this project for our Intro to Software Engineering
+                                Course. This idea stemmed from an idea that people want expensive items for cheap.
+                                We decided to make this idea into a game of chance in which everybody is satisfied.
+                                Sellers can sell items just like any other e-commerce site but buyers can save money
+                                by.
+                            </p>
                         </div>
                         <div className="row">
                             <h5>Contact Info</h5>
-                            <p>Insert contact email if there are any bugs</p>
+                            <p>yma71@ucsc.edu</p>
+                            <p>mnikaido@ucsc.edu</p>
+                            <p>rsteinwe@ucsc.edu</p>
+                            <p>jmoon15@ucsc.edu</p>
+                            <p>hyjyoon@ucsc.edu</p>
+                            <p>
+                                Please contact any of these email addresses for any questions,
+                                comments, concerns or bugs.
+                            </p>
+
                         </div>
                     </div>
                 </div>
